a72927fb8d48b07802d6af5cf7e900d27a060714
Koski84 <lamelas84@gmail.com>
2020-02-26 23:28:15 +0100

    First commit

---
+++ seed-data.js
@@ -0,0 +1,271 @@
+db.adverts.insertMany(
+  [
+    {
+      "description": "Me lo quitan de las manos!",
+      "type": "CHALET",
+      "images": [],
+      "score": 0
+    },
+    {
+      "description": "Nuevo ático céntrico recién reformado. Vivienda de lujo.",
+      "type": "PISO",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/LeSa57xyAqiF_IlnidwRIXoNrN9j-nB0Xoe_L3iCsVsfleMc-q0aKLGzYznYXqRh_hwj8wmnQDQt99IEUGoCTnKhhCvc_R-Dz9Iz00M9BA",
+              "quality": "HD"
+          }
+      ],
+      "score": 30
+    },
+    {
+      "description": "",
+      "size": 210,
+      "type": "CHALET",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/jkguSbkaV4tkU3OEvKo5ebAhX9_rAoSwxwU2g3pS5v8winD3Mnl8ZT4WYThAwqOvjm8h2RO_EG57z6Yfbv-Ek_NdWULEdD7OqCkYCdOANQ",
+              "quality": "HD"
+          }
+      ],
+      "score": 20
+    },
+    {
+      "description": "Único, céntrico, luminoso y recién reformado, parece nuevo",
+      "size": 130,
+      "type": "PISO",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/BsxXJrp7yIPs5lS_jvQMGitJcTCeMF9EDf-Yhm-Nf9ZH3uZD-zN8y4aZNsz5WnyMJHn34nasVuE32GDas8Z1TjS9-fnu3mCRss9nwdE",
+              "quality": "SD"
+          }
+      ],
+      "score": 65
+    },
+    {
+      "description": "Pisazo",
+      "size": 115,
+      "type": "PISO",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/rGJ18bTK_00_GhL77Y1UAp0evMTdXKp4jCpz4RZLRu9X-dPSFCjZRdvnvm7lp281Yxr4JH-Dw0Px4Q84Xg-I24seLKr__bXmmsFzl8lQ",
+              "quality": "SD"
+          },
+          {
+              "url": "https://lh3.googleusercontent.com/LeSa57xyAqiF_IlnidwRIXoNrN9j-nB0Xoe_L3iCsVsfleMc-q0aKLGzYznYXqRh_hwj8wmnQDQt99IEUGoCTnKhhCvc_R-Dz9Iz00M9BA",
+              "quality": "HD"
+          }
+      ],
+      "score": 75
+    },
+    {
+      "description": "Maravilloso chalet en un pequeño pueblo rural. El entorno es espectacular, las vistas magníficas. ¡Cómprelo ahora!",
+      "size": 290,
+      "type": "CHALET",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/V3r1ufJ12GoxxqfiS7KOVOKj08Zbc6URkdHUK6N_kSyLAX1RE4z2URDga0w99jo5sKRPuKS70YvXHFDAHfEb6K1HVvsZ8NnOjyFT-Xhw",
+              "quality": "SD"
+          },
+          {
+              "url": "https://lh3.googleusercontent.com/LqR6QuhSSiOwlPrKITL9AEyG2gqAPYo7SFEkyGTD9E5Tr_oTYhUsM7hwIpVxCAh1ZUcAsJcaDB4hzixnhA1H-_NdGYif5CMl1f8DliZX4A",
+              "quality": "SD"
+          }
+      ],
+      "score": 65
+    },
+    {
+      "description": "",
+      "height": "180",
+      "type": "FRIGORIFICO",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/F55IPfWuPQIFB47ku6jszZuXn9JXAN-ZdkRhTBPqdV9lQNPaMBKUpp-mp8U-AGaHqk66Wy6Vss8-Dhg6DfLLHmpp1fVkpNWKryJDEzwI",
+              "quality": "SD"
+          }
+      ],
+      "score": 50
+    },
+    {
+      "description": "Itv al día. Muy cuidado, Único dueño",
+      "km": "63000",
+      "color": "Rojo",
+      "fabricant": "BMW",
+      "type": "VEHICULO",
+      "images": [],
+      "score": 0
+    },
+    {
+      "description": "Cuidado y excepcional es frigorífico AEG",
+      "height": "180",
+      "type": "FRIGORIFICO",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/F55IPfWuPQIFB47ku6jszZuXn9JXAN-ZdkRhTBPqdV9lQNPaMBKUpp-mp8U-AGaHqk66Wy6Vss8-Dhg6DfLLHmpp1fVkpNWKryJDEzwI",
+              "quality": "SD"
+          }
+      ],
+      "score": 65
+    },
+    {
+      "description": "Ocasión!. Nuevo Audi a3",
+      "km": "0",
+      "fabricant": "Audi",
+      "type": "VEHICULO",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/V3r1ufJ12GoxxqfiS7KOVOKj08Zbc6URkdHUK6N_kSyLAX1RE4z2URDga0w99jo5sKRPuKS70YvXHFDAHfEb6K1HVvsZ8NnOjyFT-Xhw",
+              "quality": "SD"
+          },
+          {
+              "url": "https://lh3.googleusercontent.com/jkguSbkaV4tkU3OEvKo5ebAhX9_rAoSwxwU2g3pS5v8winD3Mnl8ZT4WYThAwqOvjm8h2RO_EG57z6Yfbv-Ek_NdWULEdD7OqCkYCdOANQ",
+              "quality": "HD"
+          },
+          {
+              "url": "https://lh3.googleusercontent.com/LeSa57xyAqiF_IlnidwRIXoNrN9j-nB0Xoe_L3iCsVsfleMc-q0aKLGzYznYXqRh_hwj8wmnQDQt99IEUGoCTnKhhCvc_R-Dz9Iz00M9BA",
+              "quality": "HD"
+          }
+      ],
+      "score": 65
+    },
+    {
+      "description": "Frigorífico Teka",
+      "type": "FRIGORIFICO",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/V3r1ufJ12GoxxqfiS7KOVOKj08Zbc6URkdHUK6N_kSyLAX1RE4z2URDga0w99jo5sKRPuKS70YvXHFDAHfEb6K1HVvsZ8NnOjyFT-Xhw",
+              "quality": "SD"
+          },
+          {
+              "url": "https://lh3.googleusercontent.com/F55IPfWuPQIFB47ku6jszZuXn9JXAN-ZdkRhTBPqdV9lQNPaMBKUpp-mp8U-AGaHqk66Wy6Vss8-Dhg6DfLLHmpp1fVkpNWKryJDEzwI",
+              "quality": "SD"
+          }
+      ],
+      "score": 25
+    },
+    
+
+    {
+      "description": "Gran jardín con topos!",
+      "type": "CHALET",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/F55IPfWuPQIFB47ku6jszZuXn9JXAN-ZdkRhTBPqdV9lQNPaMBKUpp-mp8U-AGaHqk66Wy6Vss8-Dhg6DfLLHmpp1fVkpNWKryJDEzwI",
+              "quality": "HD"
+          }
+      ],
+      "score": 25
+    },
+    {
+      "description": "Nuevo piso en area residencial con zonas verdes.",
+      "type": "PISO",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/LeSa57xyAqiF_IlnidwRIXoNrN9j-nB0Xoe_L3iCsVsfleMc-q0aKLGzYznYXqRh_hwj8wmnQDQt99IEUGoCTnKhhCvc_R-Dz9Iz00M9BA",
+              "quality": "SD"
+          },
+          {
+              "url": "https://lh3.googleusercontent.com/LeSa57xyAqiF_IlnidwRIXoNrN9j-nB0Xoe_L3iCsVsfleMc-q0aKLGzYznYXqRh_hwj8wmnQDQt99IEUGoCTnKhhCvc_R-Dz9Iz00M9BA",
+              "quality": "SD"
+          }
+      ],
+      "score": 30
+    },
+    {
+      "description": "Vivienda enorme! Ocasión!",
+      "size": 240,
+      "type": "CHALET",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/jkguSbkaV4tkU3OEvKo5ebAhX9_rAoSwxwU2g3pS5v8winD3Mnl8ZT4WYThAwqOvjm8h2RO_EG57z6Yfbv-Ek_NdWULEdD7OqCkYCdOANQ",
+              "quality": "HD"
+          }
+      ],
+      "score": 70
+    },
+    {
+      "description": "Excelente piso amueblado muy grande.",
+      "size": 120,
+      "type": "PISO",
+      "images": [],
+      "score": 0
+    },
+    {
+      "description": "Vivienda perfecta para una pareja",
+      "size": 110,
+      "type": "PISO",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/V3r1ufJ12GoxxqfiS7KOVOKj08Zbc6URkdHUK6N_kSyLAX1RE4z2URDga0w99jo5sKRPuKS70YvXHFDAHfEb6K1HVvsZ8NnOjyFT-Xhw",
+              "quality": "SD"
+          },
+          {
+              "url": "https://lh3.googleusercontent.com/LqR6QuhSSiOwlPrKITL9AEyG2gqAPYo7SFEkyGTD9E5Tr_oTYhUsM7hwIpVxCAh1ZUcAsJcaDB4hzixnhA1H-_NdGYif5CMl1f8DliZX4A",
+              "quality": "SD"
+          }
+      ],
+      "score": 65
+    },
+    {
+      "description": "Maravilloso chalet en una isla desierta. La tranquilidad es asombrosa.",
+      "type": "CHALET",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/V3r1ufJ12GoxxqfiS7KOVOKj08Zbc6URkdHUK6N_kSyLAX1RE4z2URDga0w99jo5sKRPuKS70YvXHFDAHfEb6K1HVvsZ8NnOjyFT-Xhw",
+              "quality": "HD"
+          },
+          {
+              "url": "https://lh3.googleusercontent.com/LqR6QuhSSiOwlPrKITL9AEyG2gqAPYo7SFEkyGTD9E5Tr_oTYhUsM7hwIpVxCAh1ZUcAsJcaDB4hzixnhA1H-_NdGYif5CMl1f8DliZX4A",
+              "quality": "HD"
+          }
+      ],
+      "score": 45
+    },
+    {
+      "description": "Viejo pero funciona!",
+      "height": "185",
+      "type": "FRIGORIFICO",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/F55IPfWuPQIFB47ku6jszZuXn9JXAN-ZdkRhTBPqdV9lQNPaMBKUpp-mp8U-AGaHqk66Wy6Vss8-Dhg6DfLLHmpp1fVkpNWKryJDEzwI",
+              "quality": "HD"
+          }
+      ],
+      "score": 65
+    },
+    {
+      "description": "Vehículo en perfectas condiciones",
+      "km": "30000",
+      "fabricant": "Mercedes",
+      "type": "VEHICULO",
+      "images": [],
+      "score": 0
+    },
+    {
+      "description": "Nuevo. Bosch.",
+      "type": "FRIGORIFICO",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/F55IPfWuPQIFB47ku6jszZuXn9JXAN-ZdkRhTBPqdV9lQNPaMBKUpp-mp8U-AGaHqk66Wy6Vss8-Dhg6DfLLHmpp1fVkpNWKryJDEzwI",
+              "quality": "HD"
+          }
+      ],
+      "score": 30
+    },
+    {
+      "description": "Ocasión!. Nuevo Skoda Octavia",
+      "km": "0",
+      "fabricant": "Skoda",
+      "type": "VEHICULO",
+      "images": [
+          {
+              "url": "https://lh3.googleusercontent.com/V3r1ufJ12GoxxqfiS7KOVOKj08Zbc6URkdHUK6N_kSyLAX1RE4z2URDga0w99jo5sKRPuKS70YvXHFDAHfEb6K1HVvsZ8NnOjyFT-Xhw",
+              "quality": "SD"
+          },
+          {
+              "url": "https://lh3.googleusercontent.com/LeSa57xyAqiF_IlnidwRIXoNrN9j-nB0Xoe_L3iCsVsfleMc-q0aKLGzYznYXqRh_hwj8wmnQDQt99IEUGoCTnKhhCvc_R-Dz9Iz00M9BA",
+              "quality": "HD"
+          }
+      ],
+      "score": 45
+    }
+])
